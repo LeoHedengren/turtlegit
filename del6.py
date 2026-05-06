a18ff9bcda60eb4a54f612179c4982cfbbd462b9
LeoH <leogabriel.hedengren@skola.taby.se>
2026-05-06 13:15:16 +0200

    lite mer

--- del6.py
+++ del6.py
@@ -1,1 +1,17 @@
 import turtle
+
+antal = int(input("Hur många figurer?"))
+
+def trianglar(antal):
+    t = turtle.Turtle()
+    t.color("blue")
+    for i in range(antal):
+        t.pendown()
+        for i in range(3):
+            t.forward(30)
+            t.right(60)
+        t.penup()
+        t.left(30)
+        t.forward(25)
+
+trianglar(antal)
