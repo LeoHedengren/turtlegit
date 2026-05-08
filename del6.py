e23ac191db603227487be1a2b99c50528d2b120a
LeoH <leogabriel.hedengren@skola.taby.se>
2026-05-08 11:09:53 +0200

    nice

--- del6.py
+++ del6.py
@@ -5,13 +5,15 @@ antal = int(input("Hur många figurer?"))
 def trianglar(antal):
     t = turtle.Turtle()
     t.color("blue")
+    t.width(7)
+    t.speed(100)
     for i in range(antal):
         t.pendown()
         for i in range(3):
             t.forward(30)
-            t.right(60)
+            t.right(120)
         t.penup()
-        t.left(30)
-        t.forward(25)
+        t.left(56)
+        t.forward(178)
 
 trianglar(antal)
