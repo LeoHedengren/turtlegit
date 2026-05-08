b453335c6c4d667d3782434ab19f947466d9d1af
LeoH <leogabriel.hedengren@skola.taby.se>
2026-05-08 11:13:54 +0200

    lite random färger nu

--- del6.py
+++ del6.py
@@ -1,15 +1,16 @@
 import turtle
+import random as rand
 
 antal = int(input("Hur många figurer?"))
 
 def trianglar(antal):
     t = turtle.Turtle()
-    t.color("blue")
     t.width(7)
     t.speed(100)
     for i in range(antal):
         t.pendown()
         for i in range(3):
+            t.color(rand.choice(["blue","cyan", "magenta", "yellow", "green", "red", "black", "white"]))
             t.forward(30)
             t.right(120)
         t.penup()
