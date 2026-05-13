16e1dfeec582c97aac5f78de3c7ec8fafe94d0bb
LeoH <leogabriel.hedengren@skola.taby.se>
2026-05-13 13:03:46 +0200

    börjat på turtlegraf

---
+++ del(göra-turtle-i-graf).py
@@ -0,0 +1,22 @@
+import turtle as t
+import math
+
+# #Rita axlar
+t.goto(-300,0); t.pendown()
+t.goto(300,0)
+t.penup()
+t.goto(0,-200); t.pendown()
+t.goto(0,200)
+
+def kordinater(x,y):
+    
+
+
+
+# # tim = t.Turtle()
+
+# # tim.forward(100)
+# # tim.right(90)
+# # tim.forward(100)
+
+# # t.done()
